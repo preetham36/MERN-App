325fdb7da1a67344331eeb357e878becaa5555db
Preetham <preetham.angadi@effiser.com>
2020-01-13 15:04:01 +0530

    Authentication Module in MERN stack

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,65 @@
+import React, { Fragment, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
+
+const Navbar = ({ title, icon }) => {
+
+    const authContext = useContext(AuthContext);
+    
+    const { isAuthenticated, logout, user } = authContext;
+
+    const onLogout = () => {
+        logout();
+    }
+
+    const authLinks = (
+        <Fragment>
+            <li>Hello { user && user.name }</li>
+            <li>
+                <a onClick={onLogout} href="#!">
+                    <i className="fa fa-sign-out-alt" />{' '}
+                    <span className="hide-sm">Logout</span>
+                </a>
+            </li>
+        </Fragment>
+    );
+
+    const guestsLinks = (
+        <Fragment>
+            <li>
+                <Link to='/register'>Register</Link>
+            </li>
+            <li>
+                <Link to='/login'>Login</Link>
+            </li>
+        </Fragment>
+    );
+
+    return (
+        <div className="navbar bg-primary">
+            <h1><i className={icon} />{title}</h1>
+            <ul>
+                {isAuthenticated ? authLinks : guestsLinks}
+                {/* <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li> */}
+            </ul>
+        </div>
+    )
+};
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+    title: 'Movie List',
+    icon: 'fa fa-film',
+};
+
+export default Navbar;
